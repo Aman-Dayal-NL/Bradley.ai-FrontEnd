be374bf87f16158d46610b424548d09a8a822385
PoulasthaMukherjee-NetraLabs <poulastha.mukherjee@netralabs.ai>
2024-11-26 16:23:51 +0530

    .

--- src/pages/Financial Info/SubStep2/Own/What Are Your Budget & Investment Goals.tsx
+++ src/pages/Financial Info/SubStep2/Own/What Are Your Budget & Investment Goals.tsx
@@ -133,6 +133,10 @@ const SubStep2: React.FC = () => {
           fontSize: '0.7rem',
           '& .MuiInputBase-root': { height: '24px', padding: '0 6px' },
           '& input': { padding: 0, fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.8rem' },
+          '& .MuiInputBase-input::placeholder': {
+            fontFamily: 'Nunito Sans, sans-serif',
+            fontSize: '0.7rem',
+          }
         }}
       />
     ))}
